23b2be0c85d8cf1a2842a7652f3a959da03ffafa
Ewerthon Santana <ewerthon32santana@gmail.com>
2021-11-27 13:26:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Dio.Helper;
+using System;
+
+namespace Dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 6, 10, 4, 7, 1 };
+
+            Console.WriteLine("Array Original:");
+            op.ImprimirArray(array);
+
+            op.OrdenarBubleSort(array);
+
+            Console.WriteLine("Array Ordenado:");
+            op.ImprimirArray(array);     
+
+        }
+    }
+}
